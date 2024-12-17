9b37f473727d57d00a90b72dab8ef9c95f7d997d
ali7haider <ali7haider.dev@gmail.com>
2024-12-18 00:54:57 +0500

    Defining matcher route and adding test resume

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app=Flask(__name__)
 @app.route("/")
 def matchReume():
     return render_template('index.html')
-
+@app.route("/matcher",methods=['GET','POST'])
+def matchReume():
+    return render_template('index.html')
 if __name__=="__main__":
     app.run(debug=True)
